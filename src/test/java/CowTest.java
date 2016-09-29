cb5c08a944e9a0ee11fdda7d0ac76b4dc3beac0c
Annika Svedin <annika.svedin@gmail.com>
2016-09-29 10:38:42 +0200

    @Before class method initialize added

--- src/test/java/CowTest.java
+++ src/test/java/CowTest.java
@@ -2,19 +2,23 @@ import org.junit.*;
 import static org.junit.Assert.assertEquals;
 
 public class CowTest {
+    private Cow cow;
+
+    @Before
+    public void initialize() {
+        this.cow = new Cow(2016, "Matilda");
+
+    }
 
     @Test
     public void createCow() {
-        Cow newCow = new Cow(2016, "Matilda");
-
-        assertEquals(2016, newCow.getBirthYear());
-
+        assertEquals(2016, this.cow.getBirthYear());
     }
 
     @Test
     public void getCowName() {
-        
-        Cow cow = new Cow(2016, "Melinda");
-        assertEquals("Melinda", cow.getName());
+        assertEquals("Matilda", this.cow.getName());
     }
+
+
 }
